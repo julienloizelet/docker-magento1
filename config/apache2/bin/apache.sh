4e84abbc8f88739d78dc4bba6481ed0dbd09a3f7
Julien Loizelet <jloizelet@sergent-major.com>
2017-09-28 16:47:01 +0200

    code cleaning

--- config/apache2/bin/apache.sh
+++ config/apache2/bin/apache.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 function setApacheVhost(){
     substitute-env-vars.sh /etc /etc/magento1.conf.tmpl;
     cp -v /etc/magento1.conf /etc/apache2/sites-enabled/magento1.conf;
@@ -11,7 +9,9 @@ function runApache(){
 if /etc/init.d/apache2 status > /dev/null;
 then
 echo "Apache already running";
-else exec /usr/sbin/apache2ctl -D FOREGROUND;
+else
+echo "Apache is not already running";
+exec /usr/sbin/apache2ctl -D FOREGROUND;
 fi
 }
 
